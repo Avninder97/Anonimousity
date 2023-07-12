9aa5c7e30ea59241067a9c3c5cd76b0c43b445e4
Avninder <avninders@argusoft.com>
2023-07-12 14:44:08 +0530

    Added Client side code and restructured the entire codebase

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.validate = async (req, res, next) => {
+    try {
+        const auth_header = req.headers.authorization;
+        if(!auth_header){
+            return res.status(401).json({
+                message: 'Unauthorized request'
+            });
+        }
+        const token = auth_header.split(' ')[1];
+        const decoded = await jwt.verify(token, process.env.SECRET_KEY);
+        console.log(decoded);
+        next();
+    }catch(err) {
+        return res.status(403).json({
+            message: 'Invalid token'
+        })
+    }
+}
