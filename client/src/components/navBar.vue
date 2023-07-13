b4c81995e6f20a8386bd4c2f0cb04f567fd11eed
prateek-kgupta <pkgupta1814@gmail.com>
2023-07-13 12:37:35 +0530

    navbar

---
+++ client/src/components/navBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">Anonymousity</a>
+      
+        <div v-if="token" class="btn-group">
+          <img
+            src="../assets/b1.png"
+            class="navbar-logo"
+            width="38"
+            data-bs-toggle="dropdown"
+            aria-expanded=""
+            alt="..."
+          />
+          <ul class="dropdown-menu dropdown-menu-end">
+            <li class="dropdown-item">Profile</li>
+            <li><hr class="dropdown-divider" /></li>
+            <li class="dropdown-item">Log Out</li>
+          </ul>
+        </div>
+        <button v-else class="btn btn-outline-success">Login / Sign Up</button>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "navBar",
+  data() {
+    return {
+      token: false,
+    };
+  },
+};
+</script>
+<style>
+.dropdown-item{
+  cursor: pointer;
+}
+
+</style>
